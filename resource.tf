d023e34414cd5b2e1d707a613efc6e2a9afb8af5
aswini-manne <aswinimanne568@gmail.com>
2025-07-16 10:40:51 +0000

    firstcommit1

--- resource.tf
+++ resource.tf
@@ -19,8 +19,8 @@ resource "azurerm_storage_container" "sacontainer" {
 resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
     address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra.location
-    resource_group_name = azurerm_resource_group.terra.name
+    location            = azurerm_resource_group.storeterra.location
+    resource_group_name = azurerm_resource_group.storeterra.name
 }
  
 resource "azurerm_subnet" "subnet1" {
