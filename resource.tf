50055d3d15efa8936900e8af72d4b7f8f06ffa9f
aswini-manne <aswinimanne568@gmail.com>
2025-07-16 10:43:15 +0000

    firstcommit1

--- resource.tf
+++ resource.tf
@@ -25,14 +25,14 @@ resource "azurerm_virtual_network" "terra_vnet" {
  
 resource "azurerm_subnet" "subnet1" {
     name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.storeterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.1.0/24"]
 }
  
 resource "azurerm_subnet" "subnet2" {
     name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra.name
+    resource_group_name  = azurerm_resource_group.storeterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.2.0/24"]
 }
